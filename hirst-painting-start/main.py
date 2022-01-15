a9c838fa83811d02be5686bc87d7189820275d83
Neun <37003479+quici9@users.noreply.github.com>
2022-01-15 05:03:34 +0000

    Initial commit

---
+++ hirst-painting-start/main.py
@@ -0,0 +1,45 @@
+# Get colors from image.jpg
+# import colorgram
+#
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
+from turtle import Turtle, Screen
+import random
+
+color_list = [(245, 243, 238), (246, 242, 244), (202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+
+
+tom = Turtle()
+screen = Screen()
+
+screen.colormode(255)
+
+tom.speed("fastest")
+tom.penup()
+tom.setheading(225)
+tom.forward(300)
+tom.setheading(0)
+max_dot = 100
+tom.hideturtle()
+
+for dot_count in range(1, max_dot + 1):
+    color = random.choice(color_list)
+    tom.dot(20, color)
+    tom.fd(50)
+    if dot_count % 10 == 0:
+        tom.setheading(90)
+        tom.forward(50)
+        tom.setheading(180)
+        tom.forward(500)
+        tom.setheading(0)
+
+
+screen.exitonclick()
